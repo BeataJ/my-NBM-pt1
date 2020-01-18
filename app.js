c4e469c9c5c7ad2b704e505860fb024f3b90f1d6
beata <beata@emc2wave.com>
2020-01-18 15:43:20 -0800

    ceate ulParent, loop warriorsGames array

--- app.js
+++ app.js
@@ -1,85 +1,92 @@
-const warriorsGames = [{
+const warriorsGames = [
+  {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 119,
       isWinner: true
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 106,
       isWinner: false
     }
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 105,
       isWinner: false
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 127,
       isWinner: true
     }
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 126,
       isWinner: true
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 85,
       isWinner: false
     }
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 92,
       isWinner: false
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 95,
       isWinner: true
     }
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 94,
       isWinner: false
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 98,
       isWinner: true
     }
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 115,
       isWinner: true
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 86,
       isWinner: false
     }
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 101,
       isWinner: true
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 92,
       isWinner: false
     }
   }
-]
+];
+
+const ulParent = document.createElement("ul");
+for (let game of warriorsGames) {
+  const { awayTeam, homeTeam } = game;
+  console.log(awayTeam.team, homeTeam.team);
+}
